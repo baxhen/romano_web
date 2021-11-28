e7db4ea8a951288b03261d2fdc5a2fb712a5b0a7
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2021-11-28 19:32:10 -0300

    fix carousel

--- components/home-sections/styles.ts
+++ components/home-sections/styles.ts
@@ -2,7 +2,7 @@ import theme from "../../src/theme";
 const styles: any = {
   root: {
     width: "90vw",
-    height: "70vh",
+    // height: "70vh",
     [theme.breakpoints.down("sm")]: {
       width: "20rem",
       height: "70vh",
